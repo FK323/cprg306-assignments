0f7e4febd47d9f3f3c9d74933671d66ccee0432a
Fazan Khan <fazan.khan@edu.sait.ca>
2025-01-27 17:06:48 -0700

    Week 2 assignment END

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Fazan Khan</h1>
+      <Link href="https://github.com/FK323/cprg306-assignments.git">
+        My Github Repository
+      </Link>
+    </main>
+  );
+}
